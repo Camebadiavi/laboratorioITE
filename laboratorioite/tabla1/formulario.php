5d3d028a1f4f4062a6bc3210cfed0d5ce18a10e2
Camebadiavi <37667541+Camebadiavi@users.noreply.github.com>
2018-04-04 14:01:45 -0500

    modificacion

---
+++ laboratorioite/tabla1/formulario.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Guardar</title>
+		</head>
+		<body>
+		<center>
+		<form action="insertar.php" method="POST"><BR></BR>
+		<input type="text" REQUIRED name="nombre" placeholder="Nombre..." values=""/><BR></BR>
+		<input type="text" REQUIRED name="paterno" placeholder="Apellido paterno..." values=""/><BR></BR>
+		<input type="text" REQUIRED name="materno" placeholder="Apellido materno..." values=""/><BR></BR>
+		<input type="submit" values="Aceptar"/>
+		
+	
+		</form>
+		</center>
+		</body>
